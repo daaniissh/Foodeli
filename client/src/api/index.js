91938a832cf037f9a7e771854345bd0db1882e9f
Danish <102005928+daaniissh@users.noreply.github.com>
2024-06-05 19:39:03 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://foodeli-server.vercel.app/api/",
 });
 
 export const UserSignUp = async (data) => await API.post("/user/signup", data);

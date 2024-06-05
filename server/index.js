5acef0389b4675ddd33f4d8d907a6caab555e56d
Danish <102005928+daaniissh@users.noreply.github.com>
2024-06-05 20:17:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import FoodRouter  from "./routers/Food.js"
 
 dotenv.config();
 const app = express();
-const corsOptions = {
-  origin: 'https://foodeli-jet.vercel.app', // allow this origin
-  optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//   origin: 'https://foodeli-jet.vercel.app', // allow this origin
+//   optionsSuccessStatus: 200
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
